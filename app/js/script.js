18cd1e9d30a861bcb49af5cde05c1ab0ebadce75
Tanya <tanyshka_zp@ukr.net>
2018-05-03 00:27:11 +0300

    added first-page with slider and login-page

---
+++ app/js/script.js
@@ -0,0 +1,23 @@
+/*
+$(document).ready(function(){
+
+    $('.my-slider').slick();
+});
+*/
+
+
+
+$(function(){
+    $('.my-slider').slick();
+
+});
+
+/*$('.my-slider').slick({
+ //                dots: false,
+ //                prevArrow: false,
+ //                nextArrow: ".slider__arrow",
+ slide: ".slides",
+ autoplay: true,
+ autoplaySpeed: 3000
+ });*/
+//     main-container-slider-block_arrow
